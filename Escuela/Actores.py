d23d92c10246671af649e360c72182e745087a45
matias453xd-dev <m4tiasll4n0s86@gmail.com>
2025-10-17 19:38:02 -0300

    Programación orientada a objetos en python

--- Escuela/Actores.py
+++ Escuela/Actores.py
@@ -19,7 +19,50 @@ class Alumno(Persona):
         self.__notas = notas
         self.__PerdidaDeCarrera = False
         
-    def AgregarNotas(self, nota):
-        # Append modifica la lista en sitio y devuelve None, no hay que reasignar
+    def AgregarNota(self, nota):
         self.__notas.append(nota)
+        print("Nota guardada")
+        
+    def BorrarNota(self, nota):
+        if(nota in self.__notas):     
+            self.__notas.remove(nota)
+            print("Nota borrada")
+        else:
+            print("Nota no encontrada")
+            
+    def GetNotas(self):
+        return self.__notas
+    
+    def EstadoCarrera(self, estado):
+        if(estado == True or estado == False):
+            self.__PerdidaDeCarrera = estado
+            
+    def GetEstado(self):
+        return self.__PerdidaDeCarrera
+
+class Profesor(Persona):
+    
+    def __init__(self, nombre, rut, telefono):
+        super().__init__(nombre, rut, telefono)
+        # Alumnos será una lista de objetos
+        self.__alumnos = []
+        
+    def AgregarAlumno(self, alumno):
+        self.__alumnos.append(alumno)
+        
+    # El profesor tendrá acceso al metodo de EstadoCarrera del alumno
+    def Reprobar(self, alumno):
+        for i in range(len(self.__alumnos)):
+            if(self.__alumnos[i] == alumno):
+                self.__alumnos[i].EstadoCarrera(False)
+                return
+        print("Alumno no encontrado")
+    def Aprobar(self, alumno):
+        for i in range(len(self.__alumnos)):
+            if(self.__alumnos[i] == alumno):
+                self.__alumnos[i].EstadoCarrera(True)
+                return
+        print("Alumno no encontrado")
+        
+            
         
--- Escuela/SimulandoNotas.py
+++ Escuela/SimulandoNotas.py
@@ -1,4 +1,4 @@
-from Actores import Profesor, Alumno, Persona
+from Actores import Profesor, Alumno
 # Crear alumnos
 alumno1 = Alumno("Matias", "21343323-9", 34819403, [6.5, 5.7, 5.0])
 alumno2 = Alumno("Juan", "30414234-k", 15467323, [6.6, 4.2, 6.0])  
@@ -11,31 +11,3 @@ profe.AgregarAlumno(alumno3)
 
 print(alumno1.MostrarDatos())
 print("Estado: "+alumno1.GetEstado)
-# Reprobar a alumno1
-
-# Crear instancias
-alumno1 = Alumno("Juan Perez", "12345678-9", "987654321", [5.5, 6.0])
-profesor1 = Profesor("Maria Gonzalez", "98765432-1", "123456789")
-
-# Mostrar datos del alumno
-print(alumno1.MostrarDatos())
-print("Notas iniciales:", alumno1.GetNotas())
-
-# Agregar una nota
-alumno1.AgregarNota(6.5)
-print("Notas después de agregar una nota:", alumno1.GetNotas())
-
-# Borrar una nota
-alumno1.BorrarNota(5.5)
-print("Notas después de borrar una nota:", alumno1.GetNotas())
-
-# Agregar alumno al profesor
-profesor1.AgregarAlumno(alumno1)
-
-# Reprobar al alumno
-profesor1.Reprobar(alumno1)
-print("Estado de carrera del alumno después de reprobar:", alumno1.GetEstado())
-
-# Aprobar al alumno
-profesor1.Aprobar(alumno1)
-print("Estado de carrera del alumno después de aprobar:", alumno1.GetEstado())
